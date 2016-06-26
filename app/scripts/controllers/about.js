3162f85ee78c379f9ed18e6a9175987fd35f07a5
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-06-26 18:19:22 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grunt192App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the grunt192App
+ */
+angular.module('grunt192App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grunt192App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the grunt192App
+ */
+angular.module('grunt192App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
